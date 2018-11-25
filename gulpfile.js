0693fdf1a881162e475b6d6206faeb6a370e01cb
polinashchegoleva <polina.cdm@gmail.com>
2018-11-25 13:02:29 -0800

    new tasks: watch, default; add browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,23 @@
 
 var gulp = require('gulp'),
     minCSS = require('gulp-clean-css'),
-    gp = require('gulp-load-plugins')();
+    gp = require('gulp-load-plugins')(),
+    browserSync = require('browser-sync').create();
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        }
+    });
+    browserSync.watch('build',browserSync.reload)
+});
 
 gulp.task('pug', function() {
     return gulp.src('src/pug/*.pug')
         .pipe(gp.pug())
         .pipe(gulp.dest('build'));
-})
+});
 
 gulp.task('sass', function() {
     return gulp.src('src/css/style.scss')
@@ -18,4 +28,14 @@ gulp.task('sass', function() {
         }))
         .pipe(minCSS())
         .pipe(gulp.dest('build/'));
-})
+});
+
+gulp.task('watch', function () {
+    gulp.watch('src/pug/index.pug',gulp.series('pug'));
+    gulp.watch('src/css/style.scss',gulp.series('sass'))
+});
+
+gulp.task('default',gulp.series(
+    gulp.parallel('pug', 'sass'),
+    gulp.parallel('watch', 'serve')
+));
