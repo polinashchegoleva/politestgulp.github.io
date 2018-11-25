a45532298b756b1c91ff2fbf3cad218f0162dfe1
polinashchegoleva <polina.cdm@gmail.com>
2018-11-25 10:32:39 -0800

    add gulp-clean-css, gulp-sass; made style.scss, style.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 'use strict';
 
 var gulp = require('gulp'),
+    minCSS = require('gulp-clean-css'),
     gp = require('gulp-load-plugins')();
 
 gulp.task('pug', function() {
     return gulp.src('src/pug/*.pug')
         .pipe(gp.pug())
         .pipe(gulp.dest('build'));
+})
+
+gulp.task('sass', function() {
+    return gulp.src('src/css/style.scss')
+        .pipe(gp.sass())
+        .pipe(minCSS())
+        .pipe(gulp.dest('build/'));
 })
